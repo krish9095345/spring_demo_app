4585e03fdb6ef76340233f8f5a4d4caebf35c2f2
SAIKRISHNA <S528152@nwmissouri.edu>
2020-03-11 12:02:48 -0500

    intialcommit

---
+++ src/main/java/com/example/spring_demo_app/SpringDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_demo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoAppApplication.class, args);
+	}
+
+}
